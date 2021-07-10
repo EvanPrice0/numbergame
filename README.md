19ecb0f47602d072235d6fed2729b0829dc815db
Evan Price <evanprice@Evans-MacBook-Pro.local>
2021-07-10 11:45:28 -0500

    got the ngondestroy working

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # game
 # game
+https://stackblitz.com/edit/angular-behaviorsubject-timer?file=src%2Fapp%2Fapp.component.html
--- frontend/src/app/tools/dragboard/dragboard.component.ts
+++ frontend/src/app/tools/dragboard/dragboard.component.ts
@@ -1,27 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem, CdkDrag } from '@angular/cdk/drag-drop';
 import { MatDialog } from '@angular/material/dialog';
 import { ResultsComponent } from 'src/app/modals/results/results.component';
-import { range } from 'rxjs';
+import { BehaviorSubject, Observable, timer } from 'rxjs';
+import { take } from 'rxjs/operators'
+import { User } from 'src/app/dbmodels/user';
 
 @Component({
   selector: 'app-dragboard',
   templateUrl: './dragboard.component.html',
   styleUrls: ['./dragboard.component.scss']
 })
-export class DragboardComponent {
+export class DragboardComponent implements OnInit, OnDestroy {
+  
+  ngOnInit(){
+    this.timed = timer(500, 500)
 
+    this.timed.subscribe(() => {
+        this.array$.pipe(take(1)).subscribe(val => {
+          this.timeNow = new Date()
+          const newArr = this.alterVar - Math.abs(Math.floor((this.start.getTime() - this.timeNow.getTime()) / 1000));
+          if (0 >= newArr) {
+            this.modaler('YOU LOSE!', 'Count Better'),
+              this.getStuffSetup();
+          }
+          this.subject.next(newArr);
+        })
+      })
+  }
+  ngOnDestroy() {
+    this.subject.complete()
+}
   constructor(private dialog: MatDialog) {
     this.getStuffSetup();
   }
+  timed?: Observable<number>;
+  user?: User;
+  alterVar: number = 15
+  subject: BehaviorSubject<any> = new BehaviorSubject<any>('');
+  array$: Observable<any> = this.subject.asObservable();
   inResults: number[] = []
   all = [];
   results = 0;
   makeshift = [];
   lives = 5;
   extra?: number[]
+  start?: Date;
+  timeNow?: Date;
+  timeElapsed?: number;
+
+
 
   getStuffSetup() {
+    this.start = new Date()
     this.makeshift = []
     let min = 10000;
     let max = 100000;
@@ -33,14 +64,14 @@ export class DragboardComponent {
     this.inResults = this.getListResult(results1);
     let randomList: number[] = this.createRandomList(this.results, this.inResults);
     this.all = this.inResults.concat(randomList)
-    this.all = this.all/* .sort((n1, n2) => n1 - n2).filter(function (elem, index, self) {
+    this.all = this.all.sort((n1, n2) => n1 - n2).filter(function (elem, index, self) {
       return index === self.indexOf(elem);
-    }); */.map((a) => ({sort: Math.random(), value: a}))
-    .sort((a, b) => a.sort - b.sort)
-    .map((a) => a.value)
+    }).map((a) => ({ sort: Math.random(), value: a }))
+      .sort((a, b) => a.sort - b.sort)
+      .map((a) => a.value)
     this.extra = []
     let y = 0
-    while (y < this.all.length/2 ) {
+    while (y < this.all.length / 2) {
       let x = this.all.pop()
       this.extra.push(x);
       y++
@@ -81,43 +112,46 @@ export class DragboardComponent {
   createRandomList = (results, list: number[]): number[] => {
     if (list.length < 5) {
       let x = Array.from({ length: 1 }, () => Math.floor(Math.random() * results))
-      let y = Array.from({ length: 1 }, () => Math.floor(Math.random() * results/2))
-      let z = Array.from({ length: 3 }, () => Math.floor(Math.random() * results/100))
+      let y = Array.from({ length: 1 }, () => Math.floor(Math.random() * results / 2))
+      let z = Array.from({ length: 3 }, () => Math.floor(Math.random() * results / 100))
       return (x.concat(y).concat(z));
     } else {
       let x = Array.from({ length: 1 }, () => Math.floor(Math.random() * results))
-      let y = Array.from({ length: 1 }, () => Math.floor(Math.random() * results/2))
-      let z = Array.from({ length: 5 }, () => Math.floor(Math.random() * results/100))
-      return (x.concat(y).concat(z));  
+      let y = Array.from({ length: 1 }, () => Math.floor(Math.random() * results / 2))
+      let z = Array.from({ length: 5 }, () => Math.floor(Math.random() * results / 100))
+      return (x.concat(y).concat(z));
     }
   }
-
 
   drop(event: CdkDragDrop<number[]>) {
     //console.log(this.results % event.item.data)
     if (this.results % event.item.data === 0) {
       this.results = this.results / (event.item.data)
       this.makeshift.push(event.item.data)
-
+      this.start = new Date();
       if (this.results == 1) {
-        this.dialog.open(ResultsComponent, {
-          data: { header: 'YOU WIN!', footer: 'WIN WIN WIN!' },
-        });
-        this.getStuffSetup();
+        this.modaler('YOU WIN!', 'You can count!'),
+          this.getStuffSetup();
       }
     }
     else {
       this.lives = this.lives - 1
+      this.alterVar = this.alterVar - 1
       if (this.lives == 0) {
-        this.dialog.open(ResultsComponent, {
-          data: { header: 'YOU LOSE!', footer: 'Count Better' },
-        });
-        this.getStuffSetup();
+        this.modaler('YOU LOSE!', 'Count Better'),
+          this.getStuffSetup();
       }
     }
   }
+  modaler(header: string, footer: string) {
+    this.dialog.open(ResultsComponent, {
+      data: { header: header, footer: footer },
+    });
+  }
+
   /** Predicate function that doesn't allow items to be dropped into a list. */
   noReturnPredicate() {
     return false;
   }
+
 }
